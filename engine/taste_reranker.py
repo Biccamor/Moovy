f4ccae0a792b0fec8fd36eb38559469fa48376aa
Fabian <fabianmoraesb@gmail.com>
2026-07-09 21:10:19 +0200

    feat: implemented ranker into pipeline

--- engine/taste_reranker.py
+++ engine/taste_reranker.py
@@ -1,30 +1,32 @@
 import numpy as np
-from database.database_setup import Movie
 
-def rrf(user_list:dict, top_movies:list, session, limit_movies: int = 15, alpha: float = 0.3):    
+def fusion_ranker(user_list: list, top_movies: list, limit_movies: int = 15, alpha: float = 0.3):    
     
+    if not top_movies:
+        return []
+
     not_taste = True
     for u in user_list:
         if u.taste_positive is not None:
             not_taste = False
-            break 
-    if not_taste: 
-        return top_movies 
+            break
+    if not_taste:
+        return top_movies
     
     embeddings = np.array([m['movie'].embedding for m in top_movies])
     
-    combined = 0
+    combined = np.zeros(len(top_movies))
     candidates = 0
     for u in user_list:
 
-        if not u.taste_positve:
-            continue 
-        candidates += 1 
+        if not u.taste_positive:
+            continue
+        candidates += 1
 
-        score = embeddings @ u.taste_positive
+        score = embeddings @ np.array(u.taste_positive)
 
-        if not u.taste_negative: 
-            score -= alpha*(embeddings@u.taste_negative)
+        if u.taste_negative:
+            score -= alpha * (embeddings @ np.array(u.taste_negative))
         combined += score
     combined /= candidates
 
